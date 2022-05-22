98c3ea475c2b1eb83ebbd9c71cd7c2ed3ee90217
Dharuni <34068379+Dharuni@users.noreply.github.com>
2022-05-22 19:28:39 +0530

    Create alert.c

---
+++ alert.c
@@ -0,0 +1,27 @@
+#include "stats.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+void emailAlerter(void)
+{
+  emailAlertCallCount = 1;
+}
+
+void ledAlerter(void)
+{
+  ledAlertCallCount = 1;
+}
+
+void check_and_alert(const float maxThreshold, alerter_funcptr alerters, Stats computedStats)
+{
+  if(computedStats.max > maxThreshold)
+  {
+    (void) (*alerters[0])();
+    (void) (*alerters[1])();
+  }
+  else
+  {
+    emailAlertCallCount = 0;
+    ledAlertCallCount = 0;
+  }
+}
