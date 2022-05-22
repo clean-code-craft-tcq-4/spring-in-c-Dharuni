63bb22be35cf1869157a1aaf223f02cb46bcbe20
Dharuni <34068379+Dharuni@users.noreply.github.com>
2022-05-22 19:41:32 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -26,14 +26,15 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
         s.min = numberset[0];
         for(i = 1; i< setlength; i++)
         {
-        if( numberset[i] > s.max)
-        {
-            s.max = numberset[i];
+            if( numberset[i] > s.max)
+            {
+                s.max = numberset[i];
+            }
+            if( numberset[i] < s.min)
+            {
+                s.min = numberset[i];
+            }
         }
-        if( numberset[i] < s.min)
-        {
-            s.min = numberset[i];
-        }                  
     }
     return s;
 }
