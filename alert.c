d2a622ccd67fdd6829c9cf50bf1408f09c5be860
Dharuni <34068379+Dharuni@users.noreply.github.com>
2022-05-22 19:55:08 +0530

    Update alert.c

--- alert.c
+++ alert.c
@@ -14,16 +14,3 @@ void ledAlerter(void)
   ledAlertCallCount = 1;
 }
 
-void check_and_alert(const float maxThreshold, alerter_funcptr alerters, Stats computedStats)
-{
-  if(computedStats.max > maxThreshold)
-  {
-    (void) (*alerters[0])();
-    (void) (*alerters[1])();
-  }
-  else
-  {
-    emailAlertCallCount = 0;
-    ledAlertCallCount = 0;
-  }
-}
